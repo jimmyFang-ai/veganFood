7da207beb80a4f0eb587d14610b49ad03ad8175c
jimmyFang-ai <pi20120413@yahoo.com.tw>
2022-05-17 16:34:59 +0800

    git init

--- gulpfile.js/envOptions.js
+++ gulpfile.js/envOptions.js
@@ -50,6 +50,9 @@ let envOptions = {
     src: [
       `${nodePath}/jquery/dist/jquery.slim.min.js`,
       `${nodePath}/bootstrap/dist/js/bootstrap.bundle.min.js`, // 已包含 popper.js
+      `${nodePath}/aos/dist/aos.js`,
+      `${nodePath}/swiper/swiper-bundle.js`,
+      `${nodePath}/sweetalert2/dist/sweetalert2.all.min.js`,
     ],
     concat: 'vendors.js',
     path: `${distPath}/assets/js`,
