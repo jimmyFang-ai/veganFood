5b2b468cbf85b8695174245fd6064afe9f2667fe
jimmyFang-ai <pi20120413@yahoo.com.tw>
2022-05-17 17:04:48 +0800

    Update 2022-05-17T09:04:44.562Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1094,7 +1094,7 @@ function toggleLoading(show) {
 
 var goTopBtn = document.querySelector("#goTopBtn"); //當使用者y軸  超過 800 出現 goTopBtn ，反之消失
 
-document.addEventListener("scroll", handleScroll); //點擊 goTopBtn監聽事件，如果scrollY != 0 的話，執行 Window.scrollTo() 內的 平滑滾動至 0
+window.addEventListener("scroll", handleScroll); //點擊 goTopBtn監聽事件，如果scrollY != 0 的話，執行 Window.scrollTo() 內的 平滑滾動至 0
 
 if (goTopBtn !== null) {
   goTopBtn.addEventListener("click", scrollGoTop);
